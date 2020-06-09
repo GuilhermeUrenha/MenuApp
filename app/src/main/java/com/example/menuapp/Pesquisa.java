b2f1c1e2f97ca8c3ee0870cbf369cbe4ea49c187
GuilhermeUrenha <guilherme.urenha@gmail.com>
2020-06-08 22:30:37 -0300

    Criação dos elementos da tela de pesquisa e função mínimamente feitas.

--- app/src/main/java/com/example/menuapp/Pesquisa.java
+++ app/src/main/java/com/example/menuapp/Pesquisa.java
@@ -2,7 +2,11 @@ package com.example.menuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Pesquisa extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class Pesquisa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesquisa);
+    }
+
+    public void searchWeb(View view) {
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        EditText edtText = (EditText)findViewById(R.id.tfPesq);
+        intent.putExtra(SearchManager.QUERY, edtText.getText());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            this.startActivity(intent);
+        }
     }
 }
